866e89d6661e706698222d86f429e0e520556dc3
LORENZO LOPES SCHROEDER <03805873000@senacrs.edu.br>
2025-10-01 11:33:30 -0300

    JHJh

---
+++ JavaScript/exemplo_01.js
@@ -0,0 +1,18 @@
+// +========================:
+// Exemplo laço de repetição
+
+let cont = 1;
+while(cont <= 5){
+    console.log("While: "+cont);
+    cont++;
+}
+cont = 1;
+do{
+    console.log("Do-While: "+cont);
+    cont++;
+} while (cont <= 5);
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+
+}
